e77216b485a3dad03ab58c72f11f7bb441730504
Developer <developer@example.com>
2026-06-27 01:05:45 +0300

    Fix Vercel deployment entrypoint and serverless handler

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import connectDB from "../DB/connect";
+import app from "../index";
+
+let dbConnected = false;
+
+const ensureDb = async () => {
+  if (!dbConnected) {
+    await connectDB();
+    dbConnected = true;
+  }
+};
+
+export default async function handler(req: any, res: any) {
+  await ensureDb();
+  return app(req, res);
+}
